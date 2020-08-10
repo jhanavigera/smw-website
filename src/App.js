02b551ff905e9a81fcefc2783e72a79b46b1384e
Angy Higgy <angyhiggy@Angys-MacBook-Air.local>
2020-08-09 17:23:53 -0700

    Header + buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-
 import { Navigation } from './components/Navigation';
+import Title from './components/Title';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+
     <BrowserRouter>
       <Navigation />
+      <Title />
     </BrowserRouter>
+
 
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import '/Users/angyhiggy/smw-website/src/css/Title.css'
+
+function Title() {
+    return (
+        <div className="Title">
+            <h1>Welcome to Skule Mental Wellness</h1>
+            <p>We are here to help you succeed mentally and academically during the 2020-2021 school year through our events and connecting you to campus resources!</p>
+
+            <Button variant="light">Campus Resources</Button>{' '}
+            <Button variant="light">Meet the Team</Button>{' '}
+            <Button variant="light">Online Activities</Button>{' '}
+        </div>
+
+        <div className=""
+    )
+}
+export default Title
