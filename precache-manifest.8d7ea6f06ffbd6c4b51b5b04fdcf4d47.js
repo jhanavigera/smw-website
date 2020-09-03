973ba272b4ba6c0dff96079540511e0f4eaeb253
YuYing_Liang <yuying.liang00@gmail.com>
2020-09-03 00:59:42 -0400

    Updates

---
+++ precache-manifest.8d7ea6f06ffbd6c4b51b5b04fdcf4d47.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb19ee07030167787bd4cab00e247b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e0e897f97ef4aa53ca1",
+    "url": "/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "c022fb5c27b5f9f8ea65",
+    "url": "/static/css/main.487beeda.chunk.css"
+  },
+  {
+    "revision": "1e0e897f97ef4aa53ca1",
+    "url": "/static/js/2.8d002eb7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d002eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c022fb5c27b5f9f8ea65",
+    "url": "/static/js/main.1c2d507a.chunk.js"
+  },
+  {
+    "revision": "0c50151962c5ed5b49b2",
+    "url": "/static/js/runtime-main.c8bae735.js"
+  },
+  {
+    "revision": "d90f885acd4f6a2c5d5b459842174917",
+    "url": "/static/media/Angy.d90f885a.png"
+  },
+  {
+    "revision": "040be63b73f0c5d2256f900aef793516",
+    "url": "/static/media/David_Bird.040be63b.jpg"
+  },
+  {
+    "revision": "c7f8f73bfbe54b2b109e524e17ab4e15",
+    "url": "/static/media/Doaa.c7f8f73b.png"
+  },
+  {
+    "revision": "8033c67d60e79eb88c7a09ef42f8a8bd",
+    "url": "/static/media/Emzhei_Chen.8033c67d.jpg"
+  },
+  {
+    "revision": "55ff032f1a3763ff59a730a9a73a41b8",
+    "url": "/static/media/Gayle_Lesmond.55ff032f.png"
+  },
+  {
+    "revision": "b2b27b2a80ee63376d8b757101c276b1",
+    "url": "/static/media/Handbook 2020 - Updated.b2b27b2a.pdf"
+  },
+  {
+    "revision": "5755717af4851b5672aba8e7f8e6421a",
+    "url": "/static/media/Handbook_Cover.5755717a.png"
+  },
+  {
+    "revision": "b4840cfe1b06d7c23ab217d37f7060a7",
+    "url": "/static/media/Jennifer_Fabro.b4840cfe.jpg"
+  },
+  {
+    "revision": "5348a0cd8baf4c49b8b30d8badebee4f",
+    "url": "/static/media/Jhanavi.5348a0cd.png"
+  },
+  {
+    "revision": "000131f52622a45ec4a4ce809d21db0a",
+    "url": "/static/media/Kayla.000131f5.png"
+  },
+  {
+    "revision": "8c5c6f441a7074c9ae66cbcd3ed00758",
+    "url": "/static/media/Leanne_Dawkins.8c5c6f44.jpg"
+  },
+  {
+    "revision": "1da1ba3dae135e9163a2023e194bcebd",
+    "url": "/static/media/Leslie_Grife.1da1ba3d.jpg"
+  },
+  {
+    "revision": "feb17f9373825b69202d85216acde32b",
+    "url": "/static/media/Melanie.feb17f93.png"
+  },
+  {
+    "revision": "e5ec7e85c1eb2c39d3cd7efaba374a4a",
+    "url": "/static/media/Mikhail_Burke.e5ec7e85.jpg"
+  },
+  {
+    "revision": "cb1806c50e7dba49038cfac54b9e5930",
+    "url": "/static/media/Peer in Crisis.cb1806c5.pdf"
+  },
+  {
+    "revision": "b7ccaf68b378537dbe40ca58aab1a2e5",
+    "url": "/static/media/PeerCrisisCover.b7ccaf68.png"
+  },
+  {
+    "revision": "9ebcf1c4293690b399fa33b9aa2baa8a",
+    "url": "/static/media/Pierina_Filipponne.9ebcf1c4.jpg"
+  },
+  {
+    "revision": "74925ccb2bd846331dea4fa3ec4760c1",
+    "url": "/static/media/Protiti.74925ccb.png"
+  },
+  {
+    "revision": "e76c3c14ecf13ee82d2cbc26a72c5ef0",
+    "url": "/static/media/Sabita.e76c3c14.png"
+  },
+  {
+    "revision": "7e91fc7dd6856958418166dcf7cb759b",
+    "url": "/static/media/Sabrin_Mohamed.7e91fc7d.jpg"
+  },
+  {
+    "revision": "2ed822a41108efaf64e6c5e41735fc43",
+    "url": "/static/media/Sharon_Brown.2ed822a4.jpg"
+  },
+  {
+    "revision": "2fa721de3be14fe7cbeb18544dcbd55b",
+    "url": "/static/media/Shayni_Curtis.2fa721de.jpg"
+  },
+  {
+    "revision": "b20ac01080ea2e0970dc2fbba91278e7",
+    "url": "/static/media/Sheral.b20ac010.png"
+  },
+  {
+    "revision": "fdc34ba69626dab58f7fa47dd682b605",
+    "url": "/static/media/Sonali.fdc34ba6.png"
+  },
+  {
+    "revision": "6c03aba3a74b0a7a20aa7eea2b233299",
+    "url": "/static/media/Stephanie.6c03aba3.png"
+  },
+  {
+    "revision": "ecee759f4543d6e97a93c41ce5be6db7",
+    "url": "/static/media/Stephen_Johns.ecee759f.jpg"
+  },
+  {
+    "revision": "d17307805258077af03d0b545742dff5",
+    "url": "/static/media/Vanessa.d1730780.png"
+  },
+  {
+    "revision": "da0a478506e5fb292ee4c409bfc60dec",
+    "url": "/static/media/Vanessa_Andres.da0a4785.jpg"
+  },
+  {
+    "revision": "4f98fd9648860573bc0373078c5cd44f",
+    "url": "/static/media/YuYing.4f98fd96.png"
+  },
+  {
+    "revision": "265957507a737139749dd62f3bafa3ef",
+    "url": "/static/media/Yuqi.26595750.png"
+  },
+  {
+    "revision": "a9d7a2d4261d1c70bb7b5deec103b560",
+    "url": "/static/media/academic1.a9d7a2d4.png"
+  },
+  {
+    "revision": "805bbfb93bfb85107493a6e1cfe2a5aa",
+    "url": "/static/media/academic2.805bbfb9.png"
+  },
+  {
+    "revision": "764689b0c2741bcad232e2a1a93b490b",
+    "url": "/static/media/academic3.764689b0.png"
+  },
+  {
+    "revision": "f92b333b778b406523f6902ca1591867",
+    "url": "/static/media/blog1.f92b333b.png"
+  },
+  {
+    "revision": "3e7d45b806800b93e628759c12b66894",
+    "url": "/static/media/blog2.3e7d45b8.png"
+  },
+  {
+    "revision": "673d51a21d968f7ebb4317b672acb2b6",
+    "url": "/static/media/blog3.673d51a2.png"
+  },
+  {
+    "revision": "35e44a047ef981470c60f4946b166251",
+    "url": "/static/media/mhw1.35e44a04.png"
+  },
+  {
+    "revision": "77be26127250a52851bb072725a74480",
+    "url": "/static/media/mhw2.77be2612.png"
+  },
+  {
+    "revision": "b469897f5cd113746c28f1a109e09ebf",
+    "url": "/static/media/mhw3.b469897f.png"
+  },
+  {
+    "revision": "7b2e2d2ebaa11f17ad6c3449218fbef5",
+    "url": "/static/media/stress1.7b2e2d2e.png"
+  },
+  {
+    "revision": "70f5014ecf08819290d453f3639b2ecf",
+    "url": "/static/media/stress2.70f5014e.png"
+  },
+  {
+    "revision": "8c3e1d1559992b81412b6d8b99b80e5f",
+    "url": "/static/media/stress3.8c3e1d15.png"
+  },
+  {
+    "revision": "be137dd8f170cc45826e6b031543024e",
+    "url": "/static/media/team1.be137dd8.png"
+  },
+  {
+    "revision": "95a83d7095a9c318ac0933597e35a053",
+    "url": "/static/media/team2.95a83d70.png"
+  },
+  {
+    "revision": "7c4a078620081f8a3a9df989f6c26471",
+    "url": "/static/media/team3.7c4a0786.png"
+  }
+]);
