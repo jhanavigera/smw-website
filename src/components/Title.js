b79b7e4568186b8bbe65ef51df8df16fde0741aa
Angy Higgy <angyhiggy@Angys-MacBook-Air.local>
2020-09-05 16:50:28 -0700

    new home page design

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,93 +7,56 @@ import handbook from '../img/Handbook_Cover.png'
 import peerCrisisCover from '../img/PeerCrisisCover.png'
 import handbookLink from '../Handbook 2020 - Updated.pdf'
 import PeerCrisis from '../Peer in Crisis.pdf'
+import Calendar from '../components/Calendar'
 
 
 function Title() {
     return (
-
-        <div style={{ width: '100%', margin: 'auto' }}>
-
-            <div className="title-container homeGradient">
-                <h2>Welcome to Skule Mental Wellness</h2>
-                <div class="row">
-                    <div className="about-container">
-                        <h3>What is SMW?</h3>
-                        <p>Skule Mental Wellness is comprised of a group engineering students that are strong advocates for mental health and wellness. We ensure that the Skule community has access to mental health resources they need. Additionally, we run events, workshops, and fun activities throughout the year to help relieve the stress that comes with being an engineering student.</p>
-                        <div class="about-buttons">
-                            <Link to="/About"><button className="btn btn-secondary">Meet the Team</button></Link>
-                            <button className="btn btn-secondary">Get Involved</button>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="row">
-                    <div class="handbook-container">
-                        <div class="handbook-cover">
-                            <img src={handbook} />
-                        </div>
-
-                        <div class="handbook-text">
-                            <h5>Presenting our Mental Wellness Handbook</h5>
-                            <p>Read for ways to reduce stress and for some useful resources!</p>
-                            <a href={handbookLink} target="_blank">
-                                <button className="btn btn-secondary">Handbook PDF</button>
-                            </a>
-                        </div>
+        <div>
+            <div className="Title-image">
+                <div className="hero-text">
+                    <h1>Welcome to Skule Mental Wellness</h1><br />
+                    <h6>We are a group engineering students that are strong advocates for mental health and wellness. We ensure that the Skule community has access to mental health resources they need. Additionally, we run events, workshops, and fun activities throughout the year to help relieve the stress that comes with being an engineering student.</h6><br />
+                    <div class="row">
+                        <div class="buttons">
+                            <a href="/MentalResources" className="button1">Mental Health Resources</a>
+                            <a href="" className="button1">Join Skule Mental Wellness</a>
+                            <a href="/AcademicResources" className="button1">Academic Resources</a>
 
 
-                    </div>
-
-                    <div class="handbook-container">
-                        <div class="handbook-cover">
-                            <img src={peerCrisisCover} />
-                        </div>
-                        <div class="handbook-text">
-                            <h5>Peer in Crisis Guide</h5>
-                            <p>Learn how you can take care of your peers that may be struggling with mental health.</p>
-                            <a href={PeerCrisis} target="_blank">
-                                <button className="btn btn-secondary">Guide PDF</button>
-                            </a>
                         </div>
                     </div>
                 </div>
+
+
+
             </div>
 
+            <div class="row">
+                <div class="events-section">
+                    <Calendar />
+                </div>
 
+                <div class="pdf-section">
+                    <h1>PDF Resources</h1>
+                    <div class="item">
+                        <img src={handbook} />
+                        <h5>SMW Handbook 2020-2021</h5>
+                        <p>Read for ways to reduce stress and for some useful resources!</p>
+                        <a href={handbookLink} class="button1">Handbook PDF</a>
+                    </div>
 
+                    <div class="item">
+                        <img src={peerCrisisCover} />
+                        <h5>Peer in Crisis Guide</h5>
+                        <p>Learn how you can take care of your peers that may be struggling with mental health.</p>
+                        <a href={PeerCrisis} class="button1">Guide PDF</a>
+                    </div>
 
-
-
-            <div className="title-container resourceGradient">
-                <h2>Resources for You</h2>
-
-                <div class="button-container">
-
-
-                    <a href="/AcademicResources">
-                        <button className="resource-button blue">
-                            <span>Academic Resources</span>
-                        </button>
-                    </a>
-
-                    <a href="/MentalResources">
-                        <button className="resource-button grey">
-                            <span>Mental Health Resources</span>
-                        </button>
-                    </a>
-
-                    <a href="/">
-                        <button className="resource-button indigo">
-                            <span>COVID-19 Resources</span>
-                        </button>
-                    </a>
 
                 </div>
             </div>
-
-        </div >
-
+        </div>
     )
 }
 
---
+++ src/config.js
@@ -0,0 +1,4 @@
+
+export const
+    GOOGLE_API_KEY = 'AIzaSyCJuGoynBIJjl7F1vLLz500mUcbr-WjO1E',
+    CALENDAR_ID = 'ahiggy03@gmail.com';
