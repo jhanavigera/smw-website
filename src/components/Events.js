73ba3a4c8d74caba985f345b7ef4b244286ada95
Angy Higgy <angyhiggy@Angys-MacBook-Air.local>
2020-09-25 07:37:48 -0700

    Event update

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,22 +5,22 @@ class Events extends Component {
     state = {
         events: [
             {
-                "name": "Digital Wellness Seminar with SCOre",
-                "date": "09/21/2020",
-                "time": "7:00 PM EST",
+                "name": "Movie Night (Animated/Disney Theme)",
+                "date": "09/26/2020",
+                "time": "10:00 AM EST & 8:00 PM EST",
                 "link": "TBD"
             },
 
             {
-                "name": "SMW Sunday Yoga",
-                "date": "09/27/2020",
-                "time": "Time TBD",
+                "name": "SMW Sunday Yoga with Angy",
+                "date": "10/04/2020",
+                "time": "7:00 PM EST",
                 "link": "TBD"
             },
             {
-                "name": "Movie Night",
-                "date": "09/26/2020",
-                "time": "Time TBD",
+                "name": "SMW Sunday Yoga with Vanessa",
+                "date": "10/18/2020",
+                "time": "7:00 PM EST",
                 "link": "TBD"
             }
         ]
