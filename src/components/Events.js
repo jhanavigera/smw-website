a3a15c0c7ddeeba3a1eb8bb0d97b5ab53dce9d99
Jhanavi Gera <jhanavigera@gmail.com>
2021-09-29 21:24:31 -0400

    took down old events + changed the calender id

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,17 +5,17 @@ class Events extends Component {
     state = {
         events: [
             {
-                "name": "SMW Sunday Yoga",
-                "date": "02/06/2021",
-                "time": "7:00 PM EST",
-                "link": "DM Us on Instagram for Zoom Info!"
+                "name": "",
+                "date": "",
+                "time": "",
+                "link": ""
             },
 
             {
-                "name": "SMW Sunday Yoga",
-                "date": "02/20/2021",
-                "time": "7:00 PM EST",
-                "link": "DM Us on Instagram for Zoom Info!"
+                "name": "",
+                "date": "",
+                "time": "",
+                "link": ""
             }
 
         ]
@@ -24,11 +24,16 @@ class Events extends Component {
         return (
             <div>
                 <h1>Upcoming Events</h1>
+                <div class="events">
+                    <br></br>
+                    <h6> Stay tuned for future events!</h6>     
+                </div>                 
                 {this.state.events.map(event => (
                     <div class="events">
+                         
                         <h6><b>{event.name}</b></h6>
                         <p>{event.date + " " + event.time}</p>
-                        <p>Zoom Link: {event.link}</p>
+                        <p> {event.link}</p>
                     </div>
 
                 ))}
